7de358fd582337629f2d92d6fba81ba51f0ec727
Jake Horban <jake.horban@gmail.com>
2020-11-23 12:21:00 -0800

    add comments

--- js/visualization.js
+++ js/visualization.js
@@ -1,48 +1,53 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
+// Immediately Invoked Function Expression to limit access to our variables
 ((() => {
 
     let second_data = null;
 
+    // read csv file
     d3.csv('data/budget.csv').then(function (data) {
-
-
+        // convert data in total amount and amount spent columns to integers
         data.forEach(function (d) {
             d.total_amount = +d.total_amount;
             d.amount_spent = +d.amount_spent;
 
         });
-
+        // save variable
         second_data = data;
     })
 
+    // read csv file
     d3.csv("data/funds.csv").then(function (data) {
+        // convert data in total amount and amount spent columns to integers
         data.forEach(function (d) {
             d.total_amount = +d.total_amount;
             d.amount_spent = +d.amount_spent;
         });
 
-        let sources = data.map(d => d.source)
-
         // General event type for selections, used by d3-dispatch
         // https://github.com/d3/d3-dispatch
-        const dispatchString = 'selectionUpdated';
+        let dispatchString = 'selectionUpdated',
 
-        // Create a line chart given x and y attributes, labels, offsets;
-        // a dispatcher (d3-dispatch) for selection events;
-        // a div id selector to put our svg in; and the data to use.
-        let total_fuel = totalBarChart()
-        ('#total-bar-chart', data, sources);
+            // get list of all sources
+            sources = data.map(d => d.source),
 
-        let source_fuel = sourceBarChart()
-            .selectionDispatcher(d3.dispatch(dispatchString))
-            ('#source-bar-chart', data, sources);
+            // total budget chart
+            total_fuel = totalBarChart()
+            ('#total-bar-chart', data, sources),
 
-        let budget_category = budgetCatBarChart()
-            ('#budget-cat-chart', second_data, sources)
+            // source bar chart
+            source_fuel = sourceBarChart()
+                .selectionDispatcher(d3.dispatch(dispatchString))
+                ('#source-bar-chart', data, sources),
 
-        // todo comment
-        source_fuel.selectionDispatcher().on(dispatchString + '.src-to-total', total_fuel.updateSelection);
-        source_fuel.selectionDispatcher().on(dispatchString + '.src-to-category', budget_category.updateSelection);
+            // budget category chart
+            budget_category = budgetCatBarChart()
+            ('#budget-cat-chart', second_data, sources);
+
+        // When the source chart selection is updated via clicks or brushing,
+        // tell the scatterplot and table to update it's selection (linking)
+        source_fuel.selectionDispatcher()
+            .on(dispatchString + '.src-to-total', total_fuel.updateSelection);
+        source_fuel.selectionDispatcher()
+            .on(dispatchString + '.src-to-category', budget_category.updateSelection);
     })
 })());
